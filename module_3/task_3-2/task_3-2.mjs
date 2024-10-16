d4f3802b60e4611af4adedd2926af59c93ec2e57
golas36 <mariusfje@uia.no>
2024-10-16 14:29:44 +0200

    Update task_3-2.mjs

--- module_3/task_3-2/task_3-2.mjs
+++ module_3/task_3-2/task_3-2.mjs
@@ -4,8 +4,40 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+const  d1 = Math.ceil( Math.random() * 6);
+const  d2 = Math.ceil( Math.random() * 6);
+const  d3 = Math.ceil( Math.random() * 6);
+const  d4 = Math.ceil( Math.random() * 6);
+const  d5 = Math.ceil( Math.random() * 6);
+const  d6 = Math.ceil( Math.random() * 6);
+printOut(`d1: `+ d1.toString());
+
+let diceThrow;
+diceThrow +=  d1.toString() + ',' ;
+diceThrow +=  d2.toString() + ',' ;
+diceThrow +=  d3.toString() + ',' ;
+diceThrow +=  d4.toString() + ',' ;
+diceThrow +=  d5.toString() + ',' ;
+diceThrow +=  d6.toString() + ',' ;
+
+printOut('diceThrow: ' + diceThrow);
+
+const count1 = (diceThrow.match(/1/g) || '').length;
+const count2 = (diceThrow.match(/2/g) || '').length;
+const count3 = (diceThrow.match(/3/g) || '').length;
+const count4 = (diceThrow.match(/4/g) || '').length;
+const count5 = (diceThrow.match(/5/g) || '').length;
+const count6 = (diceThrow.match(/6/g) || '').length;
+
+let diceCount = '';
+diceCount += count1.toString() + ',';
+diceCount += count2.toString() + ',';
+diceCount += count3.toString() + ',';
+diceCount += count4.toString() + ',';
+diceCount += count5.toString() + ',';
+diceCount += count6.toString() + ',';
+
+printOut(diceCount: ' + diceCount);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
